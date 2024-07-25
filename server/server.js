7b311949faf4507e7210c61508a92fbe81bb13d4
Dipti Jain <deeptihnd000@gmail.com>
2024-07-25 22:45:29 +0530

    deleted react extra files

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 const port = 3000;
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 app.get("/", (req, res) => {
   res.send("server started:)");
 });
+io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log(socket.id);
+});
+
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
